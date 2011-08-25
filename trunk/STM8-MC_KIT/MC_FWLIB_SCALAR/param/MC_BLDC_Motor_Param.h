6173248d0f55edba44e8abcdcb9fdb4c52448e5c
canta76@gmail.com <canta76@gmail.com@311e0fe7-882d-ec7b-d14c-d07fae61a2dd>
2011-08-25 19:02:58 +0000

    al momento bemf su ton e motore con 6 poli

--- trunk/STM8-MC_KIT/MC_FWLIB_SCALAR/param/MC_BLDC_Motor_Param.h
+++ trunk/STM8-MC_KIT/MC_FWLIB_SCALAR/param/MC_BLDC_Motor_Param.h
@@ -27,7 +27,7 @@
 #define MEASURED_ROTOR_SPEED 0 // (unit rpm)
 
 // Constant parameters
-#define MOTOR_POLE_PAIRS 2 // (unit none)
+#define MOTOR_POLE_PAIRS 6 //2 // (unit none)
 #define MAX_SPEED_RPM 3500 // (unit rpm)
 
 #endif /* __BLDC_MOTOR_PARAM_H */
