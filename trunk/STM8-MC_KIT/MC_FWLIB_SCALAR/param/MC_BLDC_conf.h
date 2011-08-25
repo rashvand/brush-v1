77ab4b57fe0978de019c45235da36a24a3e71687
canta76@gmail.com <canta76@gmail.com@311e0fe7-882d-ec7b-d14c-d07fae61a2dd>
2011-08-25 22:13:24 +0000

    tolto adc non usati sembra che l'adc ora funzioni correttamente rimosso alcuni file e porzioni di codice non usate aggiunto manuale della discovery

--- trunk/STM8-MC_KIT/MC_FWLIB_SCALAR/param/MC_BLDC_conf.h
+++ trunk/STM8-MC_KIT/MC_FWLIB_SCALAR/param/MC_BLDC_conf.h
@@ -1,25 +1,5 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : MC_BLDC_conf.h
-* Author             : IMS Systems Lab 
-* Date First Issued  : mm/dd/yyy
-* Description        : BLDC motor drive configuration
-********************************************************************************
-* History:
-* mm/dd/yyyy ver. x.y.z
-********************************************************************************
-* THE PRESENT SOFTWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*
-* THIS SOURCE CODE IS PROTECTED BY A LICENSE.
-* FOR MORE INFORMATION PLEASE CAREFULLY READ THE LICENSE AGREEMENT FILE LOCATED
-* IN THE ROOT DIRECTORY OF THIS FIRMWARE PACKAGE.
-*******************************************************************************/
+// BLDC motor drive configuration
 
-/******************************************************************************/
 #ifndef __MC_BLDC_CONF_H
 #define __MC_BLDC_CONF_H
 
@@ -37,4 +17,3 @@
 #endif
 
 #endif /*__MC_BLDC_CONF_H*/
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
--- trunk/STM8-MC_KIT/STM8_MC_FRAMEWORK/param/vdev_param.h
+++ trunk/STM8-MC_KIT/STM8_MC_FRAMEWORK/param/vdev_param.h
@@ -1,25 +1,5 @@
-/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
-* File Name          : vdev_param.h
-* Author             : IMS Systems Lab 
-* Date First Issued  : mm/dd/yyy
-* Description        : virtual device parameter file
-********************************************************************************
-* History:
-* mm/dd/yyyy ver. x.y.z
-********************************************************************************
-* THE PRESENT SOFTWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*
-* THIS SOURCE CODE IS PROTECTED BY A LICENSE.
-* FOR MORE INFORMATION PLEASE CAREFULLY READ THE LICENSE AGREEMENT FILE LOCATED
-* IN THE ROOT DIRECTORY OF THIS FIRMWARE PACKAGE.
-*******************************************************************************/
+// virtual device parameter file
 
-/******************************************************************************/
 #ifndef __VDEV_PARAM_H__
 #define __VDEV_PARAM_H__
 
@@ -138,6 +118,3 @@
 #define DISPLAY_MEM_SIZE DISPLAYCH_ADDR + DISPLAYCH_SIZE
 
 #endif /* __VDEV_H__ */
-/******************* (C) COPYRIGHT 2008 STMicroelectronics *****END OF FILE****/
-
-
